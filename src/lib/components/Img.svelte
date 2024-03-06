8327c9335b0cf4e124a0af61fe12dc11f353842d
dave morgan <dave@dmorg.org>
2024-03-06 17:46:15 +0530

    Img component

---
+++ src/lib/components/Img.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let src;
+	export let alt = '';
+	export let caption = '';
+</script>
+
+{#if alt && caption}
+	<figure>
+		<img {src} {alt} />
+		<figcaption class="caption">{caption}</figcaption>
+	</figure>
+{:else if alt}
+	<figure>
+		<img {src} {alt} />
+	</figure>
+{/if}
